63e5d02b0546fe04b3a7914266803bfa0795283e
Andrey Alekseenko <al42and@gmail.com>
2016-11-20 17:38:55 +0300

    First version, seems to work sometimes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WSDolefuls
 Working with WSD scanner from Linux
+
+Currently, it is single script that issue scan command to the device and saves resulting JPEG.
+Nothing fancy, quite contrary.
+
+Usage: `./WSD.py -H scanner.ip.address -o ouput.jpeg`
+
+For now, there's neither scanning configuration, error reporting, nor status polling.
+It sends one request, sleeps for 15 seconds (works for me), and then tries to get output.
+
+# Troubleshooting
+If for device gets scan request, it will refuse to accept further requests until someone retrieves the image.
+
+In case this program crashed/got killed, and the device is now stuck, you can either 
+a) try rebooting it, or
+b) if you remember job id, use '-j <id>` option.
+
+# Why
+I happened to buy Canon PIXMA MG3040 device, and while I wish to use it over WiFi, it only seems to support WSD scanning, not BJNP.
+
+Current code is really ugly, but there are not much alternatives to work with WSD scanners from Linux, and this script works (at least for me).
+
---
+++ WSD.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#-*- encoding: utf-8 -*-
+
+import argparse
+import email
+import requests
+import time
+import xml.etree.ElementTree as ET
+
+
+parser = argparse.ArgumentParser(description='Make Canon MG3040 scan')
+parser.add_argument('-H', nargs=1, required=True, help='Scanner ip/hostname')
+parser.add_argument('-j', nargs='?', required=False, type=int, help='Retrieve specific job instead of making a new one')
+parser.add_argument('-o', nargs=1, required=True, help='Output image file')
+
+args = parser.parse_args()
+host, jobid, output = args.H[0], args.j, args.o[0]
+print (host, jobid, output)
+
+URL='http://{}:80/wsd/scanservice.cgi'.format(host)
+
+def post_req(fname, **kwargs):
+    headers = {'content-type': 'application/soap+xml'}
+    req = ''.join(open(fname).readlines())
+    for k in kwargs:
+        req = req.replace('{{'+k+'}}', str(kwargs[k]))
+    return requests.post(URL,data=req,headers=headers)
+
+def parse_multipart(resp):
+    content_with_header = b'Content-type: ' + resp.headers['Content-Type'].encode('ascii') + resp.content
+    m = email.message_from_bytes(content_with_header)
+    return list(m.walk())
+
+def submit_job():
+    ret = post_req('submit_job.xml', URL=URL).content
+    xml = ET.fromstring(ret)
+    jobid = xml.find('{http://www.w3.org/2003/05/soap-envelope}Body').\
+                find('{http://schemas.microsoft.com/windows/2006/08/wdp/scan}CreateScanJobResponse').\
+                find('{http://schemas.microsoft.com/windows/2006/08/wdp/scan}JobId').\
+                text
+    return jobid
+
+def request_image(jobid):
+    ret = post_req('request_image.xml', URL=URL, JOBID=jobid)
+    ret = parse_multipart(ret)
+    for part in ret:
+        if part.get_content_type() == 'image/jpeg':
+            return part.get_payload(decode=True)
+    raise Exception('Can not parse response')
+
+
+if jobid is None:
+    # Submit new job
+    jobid = submit_job()
+    print ('Submitted job', jobid, ', sleeping...')
+    time.sleep(15)
+
+image_data = request_image(jobid)
+open(output, 'wb').write(image_data)
+
